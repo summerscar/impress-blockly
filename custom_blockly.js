bf1de55b4ed580905cc326797926de7ab3f7f287
summerscar <shen.yus@163.com>
2017-09-30 18:38:06 +0800

    fix

--- custom_blockly.js
+++ custom_blockly.js
@@ -2,12 +2,138 @@ Blockly.JavaScript['hello'] = function(block) {
   // TODO: Assemble JavaScript into code variable.
   var code = 
   `
-  <div class="step" data-x="0" data-y="0">
+  <div id="bored" 
+  class="step" data-x="0" data-y="0">
   Darren code - [标题]
 </div>
 <div class="step" data-x="500" data-y="0">
   This is slide 2
 </div>
+  `;
+  return code;
+};
+
+Blockly.JavaScript['impress_step'] = function(block) {
+  var number_data_x = block.getFieldValue('data-x');
+  var number_data_y = block.getFieldValue('data-y');
+  var number_data_z = block.getFieldValue('data-z');
+  var number_data_rotate_x = block.getFieldValue('data-rotate-x');
+  var number_data_rotate_y = block.getFieldValue('data-rotate-y');
+  var number_data_rotate_z = block.getFieldValue('data-rotate-z');
+  var number_data_scale = block.getFieldValue('data-scale');
+  var dropdown_font_family = block.getFieldValue('font-family');
+  var statements_step = Blockly.JavaScript.statementToCode(block, 'step');
+  // TODO: Assemble JavaScript into code variable.
+  var code = 
+  `
+    <div class="step" data-x="${number_data_x}" data-y="${number_data_y}" data-z="${number_data_z}"
+    data-rotate-x="${number_data_rotate_x}" data-rotate-y="${number_data_rotate_y}" data-rotate-z="${number_data_rotate_z}"
+    data-scale="${number_data_scale}" style="font-family:${dropdown_font_family}">
+    ${statements_step}
+    </div>
+  `;
+  return code;
+};
+
+Blockly.JavaScript['impress_start'] = function(block) {
+  var number_data_x = block.getFieldValue('data-x');
+  var number_data_y = block.getFieldValue('data-y');
+  var number_data_z = block.getFieldValue('data-z');
+  var number_data_rotate_x = block.getFieldValue('data-rotate-x');
+  var number_data_rotate_y = block.getFieldValue('data-rotate-y');
+  var number_data_rotate_z = block.getFieldValue('data-rotate-z');
+  var number_data_scale = block.getFieldValue('data-scale');
+  var dropdown_font_family = block.getFieldValue('font-family');
+  var statements_start = Blockly.JavaScript.statementToCode(block, 'start');
+  // TODO: Assemble JavaScript into code variable.
+  var code = 
+  `
+    <div id="bored" class="step" data-x="${number_data_x}" data-y="${number_data_y}" data-z="${number_data_z}"
+    data-rotate-x="${number_data_rotate_x}" data-rotate-y="${number_data_rotate_y}" data-rotate-z="${number_data_rotate_z}"
+    data-scale="${number_data_scale}" style="font-family:${dropdown_font_family}">
+    ${statements_start}
+    </div>
+  `;
+  return code;
+};
+
+Blockly.JavaScript['impress_step_slide'] = function(block) {
+  var number_data_x = block.getFieldValue('data-x');
+  var number_data_y = block.getFieldValue('data-y');
+  var number_data_z = block.getFieldValue('data-z');
+  var number_data_rotate_x = block.getFieldValue('data-rotate-x');
+  var number_data_rotate_y = block.getFieldValue('data-rotate-y');
+  var number_data_rotate_z = block.getFieldValue('data-rotate-z');
+  var number_data_scale = block.getFieldValue('data-scale');
+  var dropdown_font_family = block.getFieldValue('font-family');
+  var statements_step = Blockly.JavaScript.statementToCode(block, 'step');
+  // TODO: Assemble JavaScript into code variable.
+  var code = 
+  `
+    <div class="step slide" data-x="${number_data_x}" data-y="${number_data_y}" data-z="${number_data_z}"
+    data-rotate-x="${number_data_rotate_x}" data-rotate-y="${number_data_rotate_y}" data-rotate-z="${number_data_rotate_z}"
+    data-scale="${number_data_scale}" style="font-family:${dropdown_font_family}">
+    ${statements_step}
+    </div>
+  `;
+  return code;
+};
+
+Blockly.JavaScript['impress_block'] = function(block) {
+  var statements_block = Blockly.JavaScript.statementToCode(block, 'block');
+  // TODO: Assemble JavaScript into code variable.
+  var code = 
+  `
+  <div style="padding:50px 0;">
+  ${statements_block}
+  </div>
+  `;
+  return code;
+};
+
+Blockly.JavaScript['impress_inline'] = function(block) {
+  var text_text = block.getFieldValue('text');
+  var number_font_size = block.getFieldValue('font-size');
+  var colour_color = block.getFieldValue('color');
+  var checkbox_font_weight = block.getFieldValue('font-weight') == 'TRUE';
+  var checkbox_font_style = block.getFieldValue('font-style') == 'TRUE';
+  // TODO: Assemble JavaScript into code variable.
+  var code = 
+  `
+  <span style="color:${colour_color};font-size:${number_font_size+'px'};
+  font-weight:${checkbox_font_weight ? 'bold' : 'normal'};font-style:${checkbox_font_style ? 'italic' : 'normal'}">
+  ${text_text}
+  </span>
+  `;
+  return code;
+};
+
+Blockly.JavaScript['impress_image'] = function(block) {
+  var text_src = block.getFieldValue('src');
+  var number_width = block.getFieldValue('width');
+  var number_height = block.getFieldValue('height');
+  // TODO: Assemble JavaScript into code variable.
+  var code = 
+  `
+  <img src="${text_src}" width="${number_width ? number_width : '' }" height="${number_height ?  number_height : '' }" alt="img">
+  `;
+  return code;
+};
+
+Blockly.JavaScript['impress_step_overview'] = function(block) {
+  var number_data_x = block.getFieldValue('data-x');
+  var number_data_y = block.getFieldValue('data-y');
+  var number_data_z = block.getFieldValue('data-z');
+  var number_data_rotate_x = block.getFieldValue('data-rotate-x');
+  var number_data_rotate_y = block.getFieldValue('data-rotate-y');
+  var number_data_rotate_z = block.getFieldValue('data-rotate-z');
+  var number_data_scale = block.getFieldValue('data-scale');
+  // TODO: Assemble JavaScript into code variable.
+  var code = 
+  `
+  <div id="overview" class="step" data-x="${number_data_x}" data-y="${number_data_y}" data-z="${number_data_z}"
+  data-rotate-x="${number_data_rotate_x}" data-rotate-y="${number_data_rotate_y}" data-rotate-z="${number_data_rotate_z}"
+  data-scale="${number_data_scale}"></div>
   `;
   return code;
 };
--- custom_blocks.js
+++ custom_blocks.js
@@ -9,3 +9,171 @@ Blockly.Blocks['hello'] = {
  this.setHelpUrl("");
   }
 };
+
+Blockly.Blocks['impress_step'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("step")
+        .appendField("坐标X")
+        .appendField(new Blockly.FieldNumber(0), "data-x")
+        .appendField("Y")
+        .appendField(new Blockly.FieldNumber(0), "data-y")
+        .appendField("Z")
+        .appendField(new Blockly.FieldNumber(0), "data-z")
+        .appendField("  旋转X")
+        .appendField(new Blockly.FieldNumber(0), "data-rotate-x")
+        .appendField("Y")
+        .appendField(new Blockly.FieldNumber(0), "data-rotate-y")
+        .appendField("Z")
+        .appendField(new Blockly.FieldNumber(0), "data-rotate-z")
+        .appendField("  缩放")
+        .appendField(new Blockly.FieldNumber(1), "data-scale")
+        .appendField("字体")
+        .appendField(new Blockly.FieldDropdown([["Arial","Arial"], ["sans-serif","sans-serif"], ["微软雅黑","Microsoft YaHei"], ["黑体","黑体"], ["宋体","宋体"]]), "font-family");
+    this.appendStatementInput("step")
+        .setCheck(null);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(180);
+ this.setTooltip("场景中一幕");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['impress_start'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("start")
+        .appendField("坐标X")
+        .appendField(new Blockly.FieldNumber(0), "data-x")
+        .appendField("Y")
+        .appendField(new Blockly.FieldNumber(0), "data-y")
+        .appendField("Z")
+        .appendField(new Blockly.FieldNumber(0), "data-z")
+        .appendField("  旋转X")
+        .appendField(new Blockly.FieldNumber(0), "data-rotate-x")
+        .appendField("Y")
+        .appendField(new Blockly.FieldNumber(0), "data-rotate-y")
+        .appendField("Z")
+        .appendField(new Blockly.FieldNumber(0), "data-rotate-z")
+        .appendField("  缩放")
+        .appendField(new Blockly.FieldNumber(1), "data-scale")
+        .appendField("字体")
+        .appendField(new Blockly.FieldDropdown([["Arial","Arial"], ["sans-serif","sans-serif"], ["微软雅黑","Microsoft YaHei"], ["黑体","黑体"], ["宋体","宋体"]]), "font-family");
+    this.appendStatementInput("start")
+        .setCheck(null);
+    this.setNextStatement(true, null);
+    this.setColour(270);
+ this.setTooltip("场景中第一幕");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['impress_step_slide'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("step_slide")
+        .appendField("坐标X")
+        .appendField(new Blockly.FieldNumber(0), "data-x")
+        .appendField("Y")
+        .appendField(new Blockly.FieldNumber(0), "data-y")
+        .appendField("Z")
+        .appendField(new Blockly.FieldNumber(0), "data-z")
+        .appendField("  旋转X")
+        .appendField(new Blockly.FieldNumber(0), "data-rotate-x")
+        .appendField("Y")
+        .appendField(new Blockly.FieldNumber(0), "data-rotate-y")
+        .appendField("Z")
+        .appendField(new Blockly.FieldNumber(0), "data-rotate-z")
+        .appendField("  缩放")
+        .appendField(new Blockly.FieldNumber(1), "data-scale")
+        .appendField("字体")
+        .appendField(new Blockly.FieldDropdown([["Arial","Arial"], ["sans-serif","sans-serif"], ["微软雅黑","Microsoft YaHei"], ["黑体","黑体"], ["宋体","宋体"]]), "font-family");
+    this.appendStatementInput("step")
+        .setCheck(null);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(330);
+ this.setTooltip("场景中一幕");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['impress_block'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("行元素div");
+    this.appendStatementInput("block")
+        .setCheck(null);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(90);
+ this.setTooltip("行元素div");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['impress_inline'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("行内元素span")
+        .appendField("文本")
+        .appendField(new Blockly.FieldTextInput("示例文本"), "text")
+        .appendField(" 字体大小")
+        .appendField(new Blockly.FieldNumber(60, 12), "font-size")
+        .appendField(new Blockly.FieldColour("#000000"), "color")
+        .appendField("加粗")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "font-weight")
+        .appendField("斜体")
+        .appendField(new Blockly.FieldCheckbox("FALSE"), "font-style");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(90);
+ this.setTooltip("行内元素span");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['impress_image'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("行内元素图片")
+        .appendField("链接")
+        .appendField(new Blockly.FieldTextInput("https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png"), "src")
+        .appendField("长")
+        .appendField(new Blockly.FieldNumber(0, 0), "width")
+        .appendField("宽")
+        .appendField(new Blockly.FieldNumber(0, 0), "height");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(0);
+ this.setTooltip("行内元素image");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['impress_step_overview'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("全局预览")
+        .appendField("坐标X")
+        .appendField(new Blockly.FieldNumber(0), "data-x")
+        .appendField("Y")
+        .appendField(new Blockly.FieldNumber(0), "data-y")
+        .appendField("Z")
+        .appendField(new Blockly.FieldNumber(0), "data-z")
+        .appendField("  旋转X")
+        .appendField(new Blockly.FieldNumber(0), "data-rotate-x")
+        .appendField("Y")
+        .appendField(new Blockly.FieldNumber(0), "data-rotate-y")
+        .appendField("Z")
+        .appendField(new Blockly.FieldNumber(0), "data-rotate-z")
+        .appendField("  缩放")
+        .appendField(new Blockly.FieldNumber(1), "data-scale");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(60);
+ this.setTooltip("全局预览");
+ this.setHelpUrl("");
+  }
+};
